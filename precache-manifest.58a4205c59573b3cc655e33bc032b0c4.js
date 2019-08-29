f19260225add0209bfe36187630212be06ae569e
Simone Bogni <simone.bogni.dev@gmail.com>
2019-08-29 17:27:29 +0300

    Updates

---
+++ precache-manifest.58a4205c59573b3cc655e33bc032b0c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e23f30646be3dba564856faa56e8c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0db677f499857202c33e",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "1bf8eea6d912ae8b4cac",
+    "url": "/static/css/main.7a1cca65.chunk.css"
+  },
+  {
+    "revision": "0db677f499857202c33e",
+    "url": "/static/js/2.ecc9a3f7.chunk.js"
+  },
+  {
+    "revision": "1bf8eea6d912ae8b4cac",
+    "url": "/static/js/main.8314fc70.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "46450e285451614b69b11802492f5ba1",
+    "url": "/static/media/logo.46450e28.svg"
+  }
+]);
